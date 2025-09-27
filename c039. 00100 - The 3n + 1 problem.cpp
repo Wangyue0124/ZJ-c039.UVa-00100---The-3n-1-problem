e06abc0322b84207d161be7e59f2ee3651ed5ae0
Wangyue0124 <diego960124@gmail.com>
2025-09-28 02:39:15 +0800

    Add files via upload

---
+++ c039. 00100 - The 3n + 1 problem.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int cyclelength(int n);
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a,b;
+    while (cin >> a >> b) {
+        int sum = 0, max = 0, n = a, m = b;
+        if (a > b) swap(a, b);
+        for (int j = a; j <= b; j++) {
+            sum = cyclelength(j);
+            if ( sum > max) max = sum;
+        }
+        cout << n << " " << m << " " << max << "\n";
+    }
+    return 0;
+}
+int cyclelength(int n)
+{
+    int sum = 1;
+    while (n != 1) {
+        if (n % 2 == 1) n = n * 3 + 1;
+        else n /= 2;
+        sum++;
+    }
+    return sum;
+}
